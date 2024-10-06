053d86477ca39c2379832783545a174b7dfbfd8d
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2024-10-06 15:56:02 +0600

    rating start added

--- src/pages/CotestDetail/ContestDetail.jsx
+++ src/pages/CotestDetail/ContestDetail.jsx
@@ -3,11 +3,24 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import PrimaryBtn from "../../components/PrimaryBtn/PrimaryBtn";
 import { Helmet } from "react-helmet";
+import { useContext, useState } from "react";
+// import { Rating } from "react-simple-star-rating";
+import "./ContestDetail.css";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const ContestDetail = () => {
   const { id } = useParams();
   const axiosSecure = useAxiosSecure();
+  const {
+    user: { email },
+  } = useContext(AuthContext);
   const currentDate = new Date();
+
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+  console.log(rating, email);
+
+  // const [userRating, setUserRating] = useState(0); // Store the user's rating
 
   const {
     data: contest,
@@ -23,8 +36,29 @@ const ContestDetail = () => {
   });
 
   const contestDeadline = new Date(contest?.contestDeadline);
-
   const isDeadlineOver = currentDate.getTime() > contestDeadline.getTime();
+
+  // const mutation = useMutation(
+  //   async (newRating) => {
+  //     const res = await axiosSecure.post(`/contests/${id}/rate`, {
+  //       rating: newRating,
+  //     });
+  //     return res.data;
+  //   },
+  //   {
+  //     onSuccess: (data) => {
+  //       // Handle success, maybe fetch updated contest data
+  //       console.log("Rating submitted successfully", data);
+  //     },
+  //     onError: (error) => {
+  //       console.error("Error submitting rating", error);
+  //     },
+  //   }
+  // );
+
+  // const handleRatingChange = (rating) => {
+  //   setUserRating(rating);
+  // };
 
   if (isLoading) {
     return (
@@ -63,6 +97,7 @@ const ContestDetail = () => {
           </div>
 
           <div className="p-6">
+            {/* Contest details */}
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
               <div>
                 <p className="text-gray-700 dark:text-gray-400 text-lg mb-4">
@@ -74,7 +109,7 @@ const ContestDetail = () => {
                   {contest.prizeMoney}
                 </p>
                 <p className="text-gray-700 dark:text-gray-400 text-lg mb-4">
-                  <span className="font-bold">Contest Price: </span>$
+                  <span className="font-bold">Contest Price: </span>${" "}
                   {contest.contestPrice}
                 </p>
               </div>
@@ -94,6 +129,7 @@ const ContestDetail = () => {
               </div>
             </div>
 
+            {/* Creator details */}
             <div className="bg-gray-100 dark:bg-gray-800 p-4 rounded-lg">
               <h2 className="text-xl font-bold text-gray-800 dark:text-gray-200 mb-4">
                 Created By
@@ -108,6 +144,53 @@ const ContestDetail = () => {
               </p>
             </div>
 
+            {/* Rating System */}
+            {/* <div className="my-6">
+              <h2 className="text-xl font-bold text-gray-800 dark:text-gray-200 mb-4">
+                Rate This Contest
+              </h2>
+
+              <div className="flex justify-center items-center rating-container">
+                <Rating
+                  count={0} // Set to 5 for 5 stars
+                  value={userRating}
+                  size={40}
+                  onChange={handleRatingChange}
+                  activeColor="#ffd700"
+                  inactiveColor="#ccc"
+                />
+              </div>
+            </div> */}
+
+            {[0, 1, 2, 3, 4].map((star, index) => {
+              const currentRating = index + 1;
+              return (
+                <label key={index}>
+                  <input
+                    type="radio"
+                    disabled
+                    name="rating"
+                    value={currentRating}
+                    onChange={() => setRating(currentRating)}
+                  />
+                  <span
+                    className="star"
+                    style={{
+                      color:
+                        currentRating <= (hover || rating)
+                          ? "#ffc107"
+                          : "#e4e5e9",
+                    }}
+                    onMouseEnter={() => setHover(currentRating)}
+                    onMouseLeave={() => setHover(null)}
+                  >
+                    &#9733;
+                  </span>
+                </label>
+              );
+            })}
+
+            {/* Register or Deadline Passed */}
             <div className="flex w-full justify-center mt-8">
               {isDeadlineOver ? (
                 <p className="text-red-500 font-bold text-xl">
